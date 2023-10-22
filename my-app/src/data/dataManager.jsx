084490dacacaaa4f37757b588d832cabeb36110a
monoguitari <jesus_rodriguez@brown.edu>
2023-10-22 02:09:16 -0400

    created dataManager

---
+++ my-app/src/data/dataManager.jsx
@@ -0,0 +1,17 @@
+const MEMORY = 0
+const CARDS = 1
+const CLOCK = 2
+export default function dataManager() {
+    let Users = []; //
+    // const user = () => {
+    //     username: String,
+    //     password:  String,
+    //     token: String,
+            // stats: games[]
+    // }
+    return(
+        <div>
+
+        </div>
+    )
+}
