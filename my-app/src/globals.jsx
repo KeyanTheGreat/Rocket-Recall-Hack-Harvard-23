fd012b34ce93345eea38049f3b9d61113a163e33
Kaley Newlin <kaley_newlin@brown.edu>
2023-10-22 03:58:21 -0400

    value

--- my-app/src/globals.jsx
+++ my-app/src/globals.jsx
@@ -1,1 +1,2 @@
 export let Users = []
+export let Games = [];
--- my-app/src/pages/SageStart.jsx
+++ my-app/src/pages/SageStart.jsx
@@ -1,12 +1,9 @@
-import React, { Link } from 'react-router-dom';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './SageStart.css';
-import { Outlet } from 'react-router-dom';
 
 const Button = () => {
   const [clicked, setClicked] = useState(false);
   const [fadeIn, setFadeIn] = useState(false);
-  const [linkText, setLinkText] =useState('')
 
   const handleClick = () => {
     setClicked(true);
@@ -17,15 +14,14 @@ const Button = () => {
   }, 2000);
 
   return (
-    <div className="containerss">
+    <div className="container">
     <button
       className={`myButton ${clicked ? 'clicked' : ''} ${fadeIn ? 'fadeIn' : ''}`}
       onClick={handleClick}
     >
-    <Link to="CardManager">
+    <Link to="firsttest">
       Start Assessment
     </Link>
-    <Outlet />
     </button>
     </div>
   );
